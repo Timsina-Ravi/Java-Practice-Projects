15e025e7b5de2b90c6920a6306594fb3e516af36
Ravi Timsina <timsinaravi23@gmail.com>
2025-12-31 16:45:51 +0545

    Print even numbers till N

---
+++ 01_Basics/2_Loops/src/EvenNumbersTillN.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenNumbersTillN {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = scanner.nextInt();
+
+        for (int i =0; i <= n ; i++){
+            if( i % 2 == 0){
+                System.out.println( i);
+            }
+        }
+        scanner.close();
+    }
+}
